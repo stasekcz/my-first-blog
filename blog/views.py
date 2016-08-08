f56eb855f0af39ff2226224183df19acbcf9f818
stasekcz <stasekcz@seznam.cz>
2016-08-08 23:52:11 +0800

    Změněn kód HTML stránek

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views definition
+
+
+def post_list(request):
+    print(request)
+    return render(request, 'blog/post_list.html', {})
